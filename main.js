6abe01affd99f50d46c4bbb4dab25312d09781e5
adam <adam.thorpe.g@gmail.com>
2016-07-12 22:42:48 -0600

    Create basic entry-point program.

--- main.js
+++ main.js
@@ -3,16 +3,60 @@
 const fs = require('fs');
 const path = require('path');
 
-const zerorpc =  require('zerorpc');
+const nconf = require('nconf');
+const winston = require('winston');
 
 const validate = require('./src/validate');
+const Listener = require('./src/listener');
 const Manager = require('./src/manager');
 
-validate();
+// nconf options
+nconf.overrides({
+  'always': 'be this value'
+});
 
-let mgr = new Manager();
+nconf.argv()
+  .env()
+  .file({file: `${path.join(__dirname, 'config')}/network.json`});
 
-mgr.run();
+nconf.defaults({
+  'l': 'info'
+});
 
-// nconf.use('file', {file: `${path.join(__dirname, 'config')}/ports.json` });
-// console.log(nconf.get('name'));
+// Set up logger.
+winston.loggers.add('terminal', {
+  console: {
+    level: nconf.get('l') || 'info',
+    colorize: true,
+    label: 'terminal logger'
+  }
+});
+// var log = new winston.Logger({
+//   level: nconf.get('l'),
+//   transports: [
+//     new (winston.transports.Console)()
+//   ]
+// });
+
+function Neuron (options) {
+
+}
+
+Neuron.prototype.run = function (options) {
+  let mgr = new Manager();
+  let lstn = new Listener();
+  // main
+  lstn.start();
+  mgr.run();
+
+  lstn.stop();
+};
+
+var network = nconf.get('network');
+
+var instance = new Neuron({
+  network: network
+});
+
+instance.run();
+module.exports = instance;
